179cdebf524ad11d4f29986a14a41d10a2d6eda7
gjtinoco <genaro_13@hotmail.es>
2018-10-26 09:03:03 -0600

    Initial Commit

---
+++ precache-manifest.a26597aabadec5bb0d205e842086fc50.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "4eb3c78f890da7cc801d",
+    "url": "/static/js/main.4eb3c78f.chunk.js"
+  },
+  {
+    "revision": "475ff45f54b995c4417e",
+    "url": "/static/js/1.475ff45f.chunk.js"
+  },
+  {
+    "revision": "4eb3c78f890da7cc801d",
+    "url": "/static/css/main.dacff66f.chunk.css"
+  },
+  {
+    "revision": "ccd7877a5930e134c0f520df27836d45",
+    "url": "/index.html"
+  }
+];
